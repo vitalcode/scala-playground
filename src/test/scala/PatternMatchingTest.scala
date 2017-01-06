f80036d4a08c504cc9b4f115ac4797b63aa9109e
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-01-06 18:09:17 +0000

    Case Classes and Pattern Matching exesises and start collecting  sort algoritms

---
+++ src/test/scala/PatternMatchingTest.scala
@@ -0,0 +1,27 @@
+import org.scalatest.{FreeSpec, ShouldMatchers}
+
+class PatternMatchingTest extends FreeSpec with ShouldMatchers {
+
+  "Case Classes and Pattern Matching" in {
+
+    trait Operation
+    sealed case class Value(v: Int) extends Operation
+    sealed case class Addition(o1: Operation, o2: Operation) extends Operation
+    sealed case class Subtraction(o1: Operation, o2: Operation) extends Operation
+
+    def calculate(o: Operation): Int = o match {
+      case Value(v) => v
+      case Addition(o1, o2) => calculate(o1) + calculate(o2)
+      case Subtraction(o1, o2) => calculate(o1) - calculate(o2)
+    }
+
+    val expression = Addition(
+      Value(3),
+      Subtraction(
+        Value(10), Value(6)
+      )
+    )
+
+    calculate(expression) shouldEqual 7
+  }
+}
---
+++ src/test/scala/SortAlgorithmsTest.scala
@@ -0,0 +1,169 @@
+import org.scalatest._
+import scala.annotation.tailrec
+
+class SortAlgorithmsTest extends FreeSpec with ShouldMatchers {
+
+  "Insertion sort: O(n^2)" - {
+    "in-place comparison-based sorting algorithm, a sub-list is maintained which is always sorted" in {
+
+      def isort(xs: List[Int]): List[Int] =
+        if (xs.isEmpty) Nil
+        else insert(xs.head, isort(xs.tail))
+
+      def insert(x: Int, xs: List[Int]): List[Int] =
+        if (xs.isEmpty || x <= xs.head) x :: xs
+        else xs.head :: insert(x, xs.tail)
+
+      isort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+    }
+  }
+
+  "Insertion sort 2: O(n^2)" in {
+    //https://github.com/vkostyukov/scalacaster/blob/master/src/sort/InsertionSort.scala
+
+    def isort[A <% Ordered[A]](list: List[A]): List[A] = {
+      def sort(as: List[A], bs: List[A]): List[A] = as match {
+        case h :: t => sort(t, insert(h, bs))
+        case Nil => bs
+      }
+
+      def insert(a: A, as: List[A]): List[A] = as match {
+        case h :: t if (a > h) => h :: insert(a, t)
+        case _ => a :: as
+      }
+
+      sort(list, Nil)
+    }
+    isort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+
+  "Selection sort: O(n^2)" in {
+    // https://stackoverflow.com/questions/1672074/selection-sort-in-functional-scala
+
+    def selectionSort(xs: List[Int]) = {
+      def selectionSortHelper(xs: List[Int], accumulator: List[Int]): List[Int] =
+        if (xs.isEmpty) accumulator
+        else {
+          val ys = maximum(xs)
+          selectionSortHelper(ys.tail, ys.head :: accumulator)
+        }
+      selectionSortHelper(xs, Nil)
+    }
+
+    def maximum(xs: List[Int]): List[Int] =
+      (List(xs.head) /: xs.tail) {
+        (ys, x) =>
+          if (x > ys.head) (x :: ys)
+          else (ys.head :: x :: ys.tail)
+      }
+
+    selectionSort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+
+  "Selection sort 2: O(n^2)" in {
+    // https://rosettacode.org/wiki/Sorting_algorithms/Selection_sort
+
+    def selectionSort[T <% Ordered[T]](list: List[T]): List[T] = {
+      def remove(e: T, list: List[T]): List[T] =
+        list match {
+          case Nil => Nil
+          case x :: xs if x == e => xs
+          case x :: xs => x :: remove(e, xs)
+        }
+
+      list match {
+        case Nil => Nil
+        case _ =>
+          val min = list.min
+          min :: selectionSort(remove(min, list))
+      }
+    }
+
+    selectionSort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+
+  "Selection sort 3: O(n^2)" in {
+    //https://github.com/vkostyukov/scalacaster/blob/master/src/sort/SelectionSort.scala
+
+    def selectionSort[A <% Ordered[A]](list: List[A]): List[A] = {
+      def sort(as: List[A], bs: List[A]): List[A] = as match {
+        case h :: t => select(h, t, Nil, bs)
+        case Nil => bs
+      }
+
+      def select(m: A, as: List[A], zs: List[A], bs: List[A]): List[A] =
+        as match {
+          case h :: t =>
+            if (m > h) select(m, t, h :: zs, bs)
+            else select(h, t, m :: zs, bs)
+          case Nil => sort(zs, m :: bs)
+        }
+
+      sort(list, Nil)
+    }
+
+    selectionSort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+
+  "Merge Sort" in {
+    // http://wuciawe.github.io/scala/algorithm/2014/09/03/merge-sort-implementation-in-scala.html
+    // https://www.tutorialspoint.com/data_structures_algorithms/merge_sort_algorithm.htm
+
+    def msort[T <% Ordered[T]](xs: List[T]): List[T] = {
+
+      @tailrec
+      def merge(res: List[T], xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
+        case (_, Nil) => res.reverse ::: xs
+        case (Nil, _) => res.reverse ::: ys
+        case (x :: xs1, y :: ys1) =>
+          if (x < y) merge(x :: res, xs1, ys)
+          else merge(y :: res, xs, ys1)
+      }
+
+      val n = xs.length / 2
+      if (n == 0) xs
+      else {
+        val (ys, zs) = xs splitAt n
+        merge(Nil, msort(ys), msort(zs))
+      }
+    }
+
+    msort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+
+  "Quick sort Ο(n log n)" in {
+    // http://www.scala-lang.org/docu/files/ScalaByExample.pdf
+    // https://www.khanacademy.org/computing/computer-science/algorithms/quick-sort/a/overview-of-quicksort
+    // https://www.tutorialspoint.com/data_structures_algorithms/quick_sort_algorithm.htm
+
+    def sort(xs: Array[Int]): Array[Int] = {
+      if (xs.length <= 1) xs
+      else {
+        val pivot = xs(xs.length / 2)
+        Array.concat(
+          sort(xs filter (pivot >)),
+          xs filter (pivot ==),
+          sort(xs filter (pivot <)))
+      }
+    }
+
+    sort(Array(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual Array(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+
+  "Quick sort using Scala partition" in {
+    // http://stackoverflow.com/questions/2314526/a-generic-quicksort-in-scala
+
+    def qsort[T <% Ordered[T]](list: List[T]): List[T] = {
+      list match {
+        case Nil => Nil
+        case x :: xs =>
+          val (before, after) = xs partition (_ < x)
+          qsort(before) ++ (x :: qsort(after))
+      }
+    }
+
+    qsort(List(1, 4, 7, 3, 4, 2, 8, 9, 32, 13, 4)) shouldEqual List(1, 2, 3, 4, 4, 4, 7, 8, 9, 13, 32)
+  }
+}
+
+// https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/divide-and-conquer-algorithms
